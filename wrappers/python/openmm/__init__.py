8c191d6b5dd1c4f96a8af28ad541fb4a0e7e5dfb
Stefan Doerr <stefdoerr@gmail.com>
2025-05-29 15:50:04 +0300

    fix the directory

--- wrappers/python/openmm/__init__.py
+++ wrappers/python/openmm/__init__.py
@@ -21,7 +21,7 @@ if not os.path.exists(openmm_library_path):
     if sys.platform == 'win32':
         # Don't move this in the next if below. it should only happen on conda packages where
         # lib is not under the package root.
-        openmm_library_path = os.path.join(openmm_library_path, '..', 'Library', 'lib')
+        openmm_library_path = os.path.join(openmm_library_path, 'Library', 'lib')
 
 print(f"XXX_OPENMM_LIBRARY_PATH: {openmm_library_path}")
 if sys.platform == 'win32':
